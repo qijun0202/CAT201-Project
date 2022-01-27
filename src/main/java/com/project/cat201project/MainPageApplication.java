b0ec4c99eb4d448b2175c238d07184601a85f898
KGYeow <kokguan0819@gmail.com>
2022-01-27 22:10:45 +0800

    some function and update in controller

--- src/main/java/com/project/cat201project/MainPageApplication.java
+++ src/main/java/com/project/cat201project/MainPageApplication.java
@@ -1,15 +1,18 @@
 package com.project.cat201project;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class MainPageApplication extends Application {
+public class MainPageApplication extends Application
+{
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException
+    {
         FXMLLoader fxmlLoader = new FXMLLoader(MainPageApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
@@ -17,7 +20,48 @@ public class MainPageApplication extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch();
+    }
+
+    public void playMusic()
+    {
+
+    }
+
+    public void stopMusic()
+    {
+
+    }
+
+    public void previousMusic()
+    {
+
+    }
+
+    public void nextMusic()
+    {
+
+    }
+
+    public void resetMusic()
+    {
+
+    }
+
+    public void changeMusicSpeed(ActionEvent event)
+    {
+
+    }
+
+    public void startTimer()
+    {
+
+    }
+
+    public void cancelTimer()
+    {
+
     }
 }
--- src/main/java/com/project/cat201project/MainPageController.java
+++ src/main/java/com/project/cat201project/MainPageController.java
@@ -1,7 +1,8 @@
 package com.project.cat201project;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
 
 public class MainPageController {
     @FXML
@@ -11,4 +12,12 @@ public class MainPageController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML private Pane pane;
+    @FXML private Label songLabel;
+    @FXML private Button playBttn, pauseBttn, resetBttn, previousBttn, nextBttn;
+    @FXML private ComboBox<String> speed;
+    @FXML private Slider volume;
+    @FXML private ProgressBar songProgressBar;
+
 }
